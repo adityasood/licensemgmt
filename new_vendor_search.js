09d9fa8d3c096bd8e8a9acc0608e769c5e86c0d8
Raj Kiran Venkata Kanaka Gade <rajkiran_gade@infosys.com>
2016-08-02 16:36:33 +0530

    modified new_vendor js file

--- new_vendor_search.js
+++ new_vendor_search.js
@@ -6,7 +6,30 @@ var fs = require('fs');
 var path = require('path')
 var ObjectId = require('mongodb').ObjectID;
 var PythonShell = require('python-shell');
-var url = 'mongodb://localhost:27017/vendor';
+//var url = 'mongodb://localhost:27017/vendor';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9090,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+if (url == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    urlLabel = url = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      url += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    urlLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    url += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
 
 //var user="rajkiran"
 var dateFormat = require('dateformat');
