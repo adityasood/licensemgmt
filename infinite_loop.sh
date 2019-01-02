fafdd5e588e7d07a61707a2a9521fe6b103ed7b8
adityas <adityas@thoughtworks.com>
2019-01-02 14:38:08 +0530

    Update infinite_loop.sh

--- infinite_loop.sh
+++ infinite_loop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-for ((i=0;i<=20;i++))
-do
-  echo "Looping ... number $i";
-  sleep 3;
-done
-
-#i=20
-#while [ "$i" -le 101 ]; do
+#for ((i=0;i<=20;i++))
+#do
 #  echo "Looping ... number $i";
 #  sleep 3;
-#  i=$(( i + 1 ))
+#done
+
+i=20
+while [ "$i" -le 101 ]; do
+echo "Looping ... number $i";
+sleep 3;
+i=$(( i + 10 ))
 #done
